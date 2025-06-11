265e40da8473077d4544ec87c9490fefe1b51a99
Chapron Maxime <70152064+Maxwap@users.noreply.github.com>
2025-06-11 08:19:20 +0200

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+
 #apt install -y openssh-server
 
 sudo apt install -y chrony
-sudo systemctl start chronyd
-sudo systemctl enable chronyd
-sudo systemctl status chronyd
+sudo systemctl start chrony
+
+sudo systemctl enable chrony
+
+systemctl status --no-pager chrony
 
 sudo chronyc tracking
 sudo chronyc sources
@@ -15,6 +17,19 @@ server 0.fr.pool.ntp.org iburst\
 \nserver 3.fr.pool.net.org iburst' /etc/chrony/chrony.conf
 
 sudo grep '^server' /etc/chrony/chrony.conf
-sudo systemctl restart chrony
+
+FILE="/etc/chrony/chrony.conf"
+STRING="allow 192.168.120.0/24"
+if grep -Fxq "$STRING" "$FILE"
+then
+        echo "already exist"
+        break
+else
+        sudo echo "allow 192.168.120.0/24" >> /etc/chrony/chrony.conf
+fi
+#sudo systemctl restart chronyd
 #chrony sources -v
 
+#apt install -y postfix
+#tools ? utils
+exit
